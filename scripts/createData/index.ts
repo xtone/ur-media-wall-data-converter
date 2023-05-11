6a091ddee6052e4c9575874e3edc3235cdcd3ec4
seiji-ando <s.ando@xtone.co.jp>
2023-05-11 16:08:05 +0900

    fix: UR_MEDIAWALL-346 画像の差し替え lint対応

--- scripts/createData/index.ts
+++ scripts/createData/index.ts
@@ -12,9 +12,9 @@ writeFileSync(
   './data/json/itemList_idSort.json',
   JSON.stringify(
     data.sort((a, b) => {
-      if (a.id < b.id) return -1;
-      if (a.id > b.id) return 1;
-      return 0;
+      if (a.id < b.id) return -1
+      if (a.id > b.id) return 1
+      return 0
     }),
     undefined,
     2
